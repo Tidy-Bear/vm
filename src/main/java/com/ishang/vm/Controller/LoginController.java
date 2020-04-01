4a8fdd6f43b0322ff9e8ed27ff36209bc782140c
LinZhexian <593613870@qq.com>
2020-04-01 18:47:55 +0800

    完成简单的登录后端

--- src/main/java/com/ishang/vm/Controller/LoginController.java
+++ src/main/java/com/ishang/vm/Controller/LoginController.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.HtmlUtils;
 import com.ishang.vm.pojo.User;
-import java.util.Objects;
 
+import javax.servlet.http.HttpSession;
 @Controller
 public class LoginController {
 
@@ -17,7 +17,7 @@ public class LoginController {
     @CrossOrigin
     @PostMapping(value = "/api/login")
     @ResponseBody
-    public Result login(@RequestBody User requestUser) {
+    public Result login(@RequestBody User requestUser, HttpSession session) {
         String username = requestUser.getUsername();
         username = HtmlUtils.htmlEscape(username);
 
@@ -25,6 +25,7 @@ public class LoginController {
         if (null == user) {
             return new Result(400);
         } else {
+            session.setAttribute("user", user);
             return new Result(200);
         }
     }
